3d0dd0bf7fec3278e522c67d2a6b579a651b90bb
Neil Berg <neil.berg14@gmail.com>
2019-02-16 17:30:24 -0800

    full revision 2/16

--- 2019-MLACKEY-002/scripts/script.js
+++ 2019-MLACKEY-002/scripts/script.js
@@ -1,18 +1,40 @@
 // Event listeners on past work buttons to reveal more text
-const pastWorkSection = document.querySelector('.past-work');
+const pastWorkSection = document.querySelector('.work');
 
 function revealText(e) {
-  if (e.target.nodeName === 'BUTTON') {
-    // Show the information in the div above targeted button
-    const previousEl = e.target.previousElementSibling  
+  if (e.target.classList.contains('fas')) {
+    // Show the information in the div above targeted icon
+    const previousEl = e.target.previousElementSibling;
     previousEl.classList.toggle('reveal');
-    // Then change the text of targeted button
+    // Then change the icon
     if (previousEl.classList.contains('reveal')) {
-      e.target.innerHTML = `Show less <i class="fas fa-angle-double-up"></i>`;
+      e.target.classList.add('fa-angle-double-up');
+      e.target.classList.remove('fa-angle-double-right');
     } else {
-      e.target.innerHTML = `Read more <i class="fas fa-angle-double-right"></i>`;
+      e.target.classList.add('fa-angle-double-right');
+      e.target.classList.remove('fa-angle-double-up');
     }
   }
 }
 
+// Determine number of thumbnails based on viewport height
+function createGallery(e) {
+  const height = window.innerHeight;
+  const numBoxes = Math.floor(height / 80) - 1;
+  const gallery = document.querySelector('.gallery');
+
+  gallery.innerHTML = Array(numBoxes)
+    .fill()
+    .map(item => {
+      return `
+      <img src="images/cablestreet_sq_${Math.floor(Math.random() * 6 + 1)}.jpg" 
+          alt="" 
+          class="gallery__image" 
+      />`;
+    })
+    .join('');
+}
+
+window.addEventListener('DOMContentLoaded', createGallery);
+window.addEventListener('resize', createGallery);
 pastWorkSection.addEventListener('click', revealText);
